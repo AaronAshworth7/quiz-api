af50872d78763c7e728e644a1137dc2782fd6e4f
Demdun <Demdun47@gmail.com>
2024-04-02 15:46:39 +0100

    message

--- src/services/quiz.js
+++ src/services/quiz.js
@@ -277,7 +277,7 @@ async function submitAnswerAsync(roomCode, playerName, answer) {
 
 function start(instance) {
     if (instance.players.length <= 2) {
-        throw new InvalidOperationError('Not enough players yet');
+        throw new InvalidOperationError('Not enough players yet! You need at least 2.');
     }
     instance.questionNumber = 1;
     instance.players.forEach(p => {
